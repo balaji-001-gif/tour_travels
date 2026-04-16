2e70c87b1139f287fe2ae63a714a1c453e776ae2
balaji-001-gif <balaji.k@seria.in>
2026-04-16 16:28:07 +0530

    Disable sidebar and allow guest access in context

--- travel_tour/www/travel_enquiry.py
+++ travel_tour/www/travel_enquiry.py
@@ -2,6 +2,8 @@ import frappe
 
 def get_context(context):
     context.no_cache = 1
+    context.show_sidebar = False
+    # Allow guest access
     context.packages = frappe.get_all(
         'Tour Package',
         fields=['name', 'package_name', 'tour_type', 'destination', 'duration_days', 'duration_nights'],
