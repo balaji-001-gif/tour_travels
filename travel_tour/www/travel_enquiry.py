ae4d9fdcbef8f927d98733935116a8db4a621ef4
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-04-16 16:14:36 +0530

    fix(portal): add ignore_permissions to Guest-facing package query

--- travel_tour/www/travel_enquiry.py
+++ travel_tour/www/travel_enquiry.py
@@ -5,5 +5,6 @@ def get_context(context):
     context.packages = frappe.get_all(
         'Tour Package',
         fields=['name', 'package_name', 'tour_type', 'destination', 'duration_days', 'duration_nights'],
-        order_by='package_name asc'
+        order_by='package_name asc',
+        ignore_permissions=True
     )
